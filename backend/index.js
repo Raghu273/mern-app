828724b9e4ea9ef73eb8ef47a14c42906d275eff
Raghu273 <95980427+Raghu273@users.noreply.github.com>
2025-02-16 20:22:26 +0530

    Update and rename server.js to index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const os = require('os');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const hostname = os.hostname();
+    res.send(`Hi this is from ${hostname}, Raghu`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
